e4c340d5ee1fd5b720711c71c3f8c704b677f41b
tam <cherykim123@gmail.com>
2021-11-03 16:04:16 -0700

    adding tag

--- src/AddTagForm.js
+++ src/AddTagForm.js
@@ -2,11 +2,10 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import useInputState from "./hook/useInputState";
 
-function AddTagForm({ addTag, showTag }) {
+function AddTagForm({ addTag }) {
   const [value, handleChange, reset] = useInputState([]);
   return (
     <div>
-      <div>{showTag}</div>
       <form
         style={{ border: "none", width: "10rem" }}
         onSubmit={(e) => {
